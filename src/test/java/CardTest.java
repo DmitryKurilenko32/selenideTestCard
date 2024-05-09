c027abaad82b81783c61493a4f3f39c3abb5e64c
Dmitry <kurilenko907@gmail.com>
2024-05-09 21:11:54 +0300

    initiall commit

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardTest {
+    @Test
+    void shouldTest() {
+        open("http://0.0.0.0:9999");
+    }
+}
